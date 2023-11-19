cd0ad0d27267bc341fb6defdd4a1ea08d8707c27
Lara Prado <laraprado.e@gmail.com>
2023-11-19 04:01:11 -0300

    Agregué descripciones de cada item

--- src/components/Items/ItemListContainer.jsx
+++ src/components/Items/ItemListContainer.jsx
@@ -5,12 +5,12 @@ import { useParams } from 'react-router-dom'
 
 const ItemListContainer = () => {
   const productos = [
-    {id:1, producto: "Crema humectante", descripcion: "", precio: 10000, categoria: "500ml", imagen : "/src/assets/humectante.jpg"},
-    {id:2, producto: "Crema humectante", descripcion: "", precio: 5000, categoria: "250ml", imagen : "/src/assets/humectante.jpg"},
-    {id:3, producto: "Gel de ducha", descripcion: "", precio: 10000, categoria: "500ml",imagen : "/src/assets/gelDucha.jpg"},
-    {id:4, producto: "Gel de ducha", descripcion: "", precio: 5000, categoria: "250ml",imagen : "/src/assets/gelDucha.jpg"},
-    {id:5, producto: "Exfoliante", descripcion: "", precio: 10000, categoria: "500ml",imagen : "/src/assets/exfoliante.jpg"},
-    {id:6, producto: "Exfoliante", descripcion: "", precio: 5000, categoria: "250ml",imagen : "/src/assets/exfoliante.jpg"},
+    {id:1, producto: "Crema humectante", descripcion: " Tamaño grande. Puede utilizarse todos los días. Composición natural e hipoalergénica.", precio: 10000, categoria: "500ml", imagen : "/src/assets/humectante.jpg"},
+    {id:2, producto: "Crema humectante", descripcion: "Tamaño chico. Puede utilizarse todos los días. Composición natural e hipoalergénica.", precio: 5000, categoria: "250ml", imagen : "/src/assets/humectante.jpg"},
+    {id:3, producto: "Gel de ducha", descripcion: "Tamaño grande. Se utiliza durante o luego del baño. ", precio: 10000, categoria: "500ml",imagen : "/src/assets/gelDucha.jpg"},
+    {id:4, producto: "Gel de ducha", descripcion: "Tamaño chico. Se utiliza durante o luego del baño.", precio: 5000, categoria: "250ml",imagen : "/src/assets/gelDucha.jpg"},
+    {id:5, producto: "Exfoliante", descripcion: "Tamaño grande. Solo se utiliza una o dos veces por semana. Ideal para eliminar impurezas.", precio: 10000, categoria: "500ml",imagen : "/src/assets/exfoliante.jpg"},
+    {id:6, producto: "Exfoliante", descripcion: "Tamaño chico. Solo se utiliza una o dos veces por semana. Ideal para eliminar impurezas.", precio: 5000, categoria: "250ml",imagen : "/src/assets/exfoliante.jpg"},
     
 
   ]
--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -3,10 +3,10 @@ import {useState} from 'react'
 const useCounter = (initialValue = 0, step = 1) => {
     const [count,setCount] = useState(initialValue)
     const increment = () => {
-        setCount((prevCount) => prevCount + step)
+        setCount((prevCount) => ( prevCount < 10) ? prevCount + step : prevCount)
     }
     const decrement = () => {
-        setCount((prevCount) => prevCount - step)
+        setCount((prevCount) => prevCount>0 ? prevCount - step: prevCount)
     }
     return{
         count, increment, decrement
